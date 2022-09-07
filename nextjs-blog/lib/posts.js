cccb6cff83a4f4e23c426fd216668325f5f9e14f
Ayu Nakata <mieze018@users.noreply.github.com>
2022-09-08 03:28:01 +0900

    外部データに依存するページパス getStaticPathsの実装
    Next.jsでは、外部データに依存するパスをもつページを静的に生成することができます。これにより、Next.jsでは動的なURLが実現されます。
    
    重要: 返されるリストは単なる文字列の配列ではなく、コメントのようなオブジェクトの配列である必要があります。各オブジェクトはparamsキーを持ち、idキーを持つオブジェクトを含んでいなければなりません (ファイル名に[id]を使っているからです)。そうでなければ、getStaticPathsは失敗します。
    
      // [
      //   {
      //     params: {
      //       id: 'ssg-ssr'
      //     }
      //   },
      //   {
      //     params: {
      //       id: 'pre-rendering'
      //     }
      //   }
      // ]

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -69,3 +69,28 @@ function Profile() {
   if (!data) return <div>loading...</div>;
   return <div>hello {data.name}!</div>;
 }
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ''),
+      },
+    };
+  });
+}
